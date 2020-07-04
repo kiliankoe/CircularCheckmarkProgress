71e812d7eecc53a6dd16656d634bdec35d543f32
Kilian Koeltzsch <me@kilian.io>
2020-07-05 01:32:52 +0200

    add link to stackoverflow answer

--- Sources/CircularCheckmarkProgress/View+foreground.swift
+++ Sources/CircularCheckmarkProgress/View+foreground.swift
@@ -1,5 +1,7 @@
 import SwiftUI
 
+// see https://stackoverflow.com/a/59858089/1843020
+
 public extension View {
     func foreground<Overlay: View>(_ overlay: Overlay) -> some View {
         // The padding here is unfortunate, can this be fixed somehow?
